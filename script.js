616f21213924e1d2374acf684b06c71e0033f965
RoyD929 <roydavid929@gmail.com>
2025-06-25 15:28:47 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function () {
-  const welcomeText = document.getElementById("welcomeText");
-  const scrollY = window.scrollY;
-  if (scrollY > 50) {
-    welcomeText.style.opacity = 0;
-  } else {
-    welcomeText.style.opacity = 1;
-  }
-});
